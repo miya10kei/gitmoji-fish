a24f1d805659ebd012d92feba2b49ff8ee1674a6
miya10kei <miya10kei@gmail.com>
2021-07-07 22:02:34 +0900

    :children_crossing: Support FZF_DEFAULT_OPTS

--- functions/gitmoji_fish.fish
+++ functions/gitmoji_fish.fish
@@ -14,12 +14,19 @@ function gitmoji_fish
     return 1
   end
 
-  set -l SHORTCODE (cat $CACHE_DIR/gitmojis.json | jq -r '.gitmojis[] | [.emoji , .code , .description] | @tsv' |  column -ts\t | fzf | awk '{ print($2) }')
-  if test -z $SHORTCODE
-    return 0
+  set -l CMD "
+    cat $CACHE_DIR/gitmojis.json \
+    | jq -r '.gitmojis[] | [.emoji , .code , .description] | @tsv' \
+    | column -ts\t \
+    | fzf $FZF_DEFAULT_OPTS \
+    | awk '{ print(\$2) }'
+  "
+  set -l SHORTCODE (eval $CMD)
+  if test -n "$SHORTCODE"
+    set -l POSITION (commandline -C)
+    commandline -i $SHORTCODE
+    commandline -C (math $POSITION + (string length $SHORTCODE))
   end
 
-  set -l POSITION (commandline -C)
-  commandline -i $SHORTCODE
-  commandline -C (math $POSITION + (string length $SHORTCODE))
+  commandline -f repaint
 end
